86f4962443c3c19aebe9065666fe0040cb45b24f
M0nss12 <monsswhat@gmail.com>
2025-11-05 12:29:45 +0800

    Fix MakerBundle for production deployment

--- clevercloud/builder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Убедитесь что dev бандлы не загружаются в production
-sed -i "/MakerBundle.*all/d" config/bundles.php
-
-# Очистка кеша для production
-php bin/console cache:clear --env=prod --no-debug
